888e5be04cfca2f2eedbce291b69a9796b21da99
jskim-research <wltnwkd21@gmail.com>
2022-06-16 12:57:48 +0900

    Test: TFRecord 테스트 코드 구현
    아직은 버그가 있으므로 테스트 더 해야함

--- src/datasets.py
+++ src/datasets.py
@@ -11,6 +11,7 @@ import typing
 import os
 import cv2
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 def cifar10() -> typing.Tuple[np.ndarray, typing.List[int], typing.List[str]]:
@@ -93,6 +94,82 @@ def cifar100() -> typing.Tuple[np.ndarray, np.ndarray, np.ndarray,
     return (train_data, train_fine_labels, train_coarse_labels,
             test_data, test_fine_labels, test_coarse_labels,
             fine_label_names, coarse_label_names)
+
+
+def image_feature(value):
+    return tf.train.Feature(
+        bytes_list=tf.train.BytesList(value=[tf.io.encode_jpeg(value).numpy()])  
+        # encode를 해서 저장하므로 메모리를 좀 줄일 수 있을 듯
+    )
+
+
+def bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value.encode("utf-8")]))
+
+
+def int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def create_example(image, path, file_name):
+    feature = {
+        # "image": image_feature(image),
+        "path": bytes_feature(path),
+        "filename": bytes_feature(file_name),
+    }
+    return tf.train.Example(features=tf.train.Features(feature=feature))
+
+
+def parse_tfrecord_fn(example):
+    feature_description = {
+        # "image": tf.io.FixedLenFeature([], tf.string),
+        "path": tf.io.FixedLenFeature([], tf.string),
+        "filename": tf.io.FixedLenFeature([], tf.string),
+    }
+    example = tf.io.parse_single_example(example, feature_description)
+    # example["image"] = tf.io.decode_jpeg(example["image"], channels=3)
+    return example
+
+
+def Make_TFRecord(file_path: str, save_path: str, num_samples: int) -> None:
+    """Make TFRecord of dogs-cats dataset
+
+    Args:
+        file_path: where to load
+        save_path: where to save
+        num_samples: number of samples in each TFRecord file
+    Returns:
+        return nothing
+    """
+    file_list = os.listdir(file_path)
+    num_tfrecords = len(file_list) // num_samples
+    if len(file_list) % num_samples:
+        num_tfrecords += 1  # add one recorde if there are any remaining samples
+
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+
+    for tfrec_num in range(num_tfrecords):
+        samples = file_list[(tfrec_num * num_samples):((tfrec_num+1) * num_samples)]
+        with tf.io.TFRecordWriter(save_path + "file_%.2i-%i.tfrec" % (tfrec_num, len(file_list))) as writer:
+            for sample in samples:  # sample = file_name
+                image = tf.io.decode_jpeg(tf.io.read_file(file_path + sample))
+                example = create_example(image, file_path, sample)
+                writer.write(example.SerializeToString())
+
+    raw_dataset = tf.data.TFRecordDataset(f"{file_path}/file_00-{num_samples}")
+    parsed_dataset = raw_dataset.map(parse_tfrecord_fn)
+
+    for features in parsed_dataset.take(1):
+        print(features)
+        for key in features.keys():
+            if key != "image":
+                print(f"{key}: {features[key]}")
+
+        # print(f"Image shape: {features['image'].shape}")
+        # plt.figure(figsize=(7, 7))
+        # plt.imshow(features["image"].numpy())
+        # plt.show()
 
 
 def dogs_cats(training: bool) -> typing.Tuple[tf.data.Dataset, int]:
