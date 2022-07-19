2fbd175ad14273f59baff56364ff38f7c17c278c
jskim-research <wltnwkd21@gmail.com>
2022-07-19 11:40:14 +0900

    Test: resnet 모델 테스트 케이스 추가

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,10 +11,16 @@ pytest option (https://velog.io/@sangyeon217/pytest-execution)
 - k 옵션
     - pytest {테스트파일명}.py -k {테스트함수명} // 특정 테스트 함수만 실행
 - v 옵션
-    - 기본 실행 명령어는 Fail => F, pass => . 으로만 표시하는데 여기선 각 테스트 함수 실행 결과 출력
+    - 기본 실행 명령어는 Fail => F, pass => . 으로만 표시하는데 여기선 각 테스트 함수 실행 결과 출력 (아마 fail 시에만)
 - vv 옵션
+    - 더 자세한 실행 결과 설명
 - s 옵션
+    - Failed 건에 대한 stdout, stderr 메세지 캡쳐 기능 비활성화 (== --capture=no)
 - r 옵션
+    - short test summary info에 나올 정보 지정 (e.g., 디폴트 옵션 -rfE는 [f]ailed 와 [E]rror 건 출력
+- --capture
+    - Passed 건에 대해 stdout, stderr 메세지 캡쳐하고 싶으면 --capture=tee-sys 사용
+- --disable-pytest-warnings // deprecate 등의 warning 출력 생략, 그러나 warning이 있다는 것은 계속 알려줌
 
 중복 class 공유 방법
 
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,33 @@
-def test_something1():
-    print("Hello!!!!!!!")
-    assert 1 == 2
+import tensorflow as tf
+from src.models import Resnet, Resnet_update
+
+
+def test_resnet():
+    """resnet 모델 검증"""
+    resnet18 = Resnet.resnet_18()
+    resnet34 = Resnet.resnet_34()
+    resnet50 = Resnet.resnet_50()
+    resnet101 = Resnet.resnet_101()
+    resnet152 = Resnet.resnet_152()
+
+    func_resnet18 = Resnet_update.resnet18()
+    func_resnet34 = Resnet_update.resnet34()
+    func_resnet50 = Resnet_update.resnet50()
+    func_resnet101 = Resnet_update.resnet101()
+    func_resnet152 = Resnet_update.resnet152()
+
+    batch_num = 8
+    tmp_input = tf.random.normal((batch_num, 224, 224, 3))  # (batch #, H, W, C)
+
+    # ImageNet 대상이므로 1000개의 class에 대한 결과가 나와야 함
+    assert resnet18(tmp_input).shape == tf.TensorShape([batch_num, 1, 1, 1000])
+    assert resnet34(tmp_input).shape == tf.TensorShape([batch_num, 1, 1, 1000])
+    assert resnet50(tmp_input).shape == tf.TensorShape([batch_num, 1, 1, 1000])
+    assert resnet101(tmp_input).shape == tf.TensorShape([batch_num, 1, 1, 1000])
+    assert resnet152(tmp_input).shape == tf.TensorShape([batch_num, 1, 1, 1000])
+    assert func_resnet18(tmp_input).shape == tf.TensorShape([batch_num, 1, 1, 1000])
+    assert func_resnet34(tmp_input).shape == tf.TensorShape([batch_num, 1, 1, 1000])
+    assert func_resnet50(tmp_input).shape == tf.TensorShape([batch_num, 1, 1, 1000])
+    assert func_resnet101(tmp_input).shape == tf.TensorShape([batch_num, 1, 1, 1000])
+    assert func_resnet152(tmp_input).shape == tf.TensorShape([batch_num, 1, 1, 1000])
+
