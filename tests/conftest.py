86c38aed2cd8b094924c8adc6d3cd81e715ae419
jskim-research <wltnwkd21@gmail.com>
2022-07-19 10:21:35 +0900

    Test: pytest를 위한 기본 코드들 구현

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,20 @@
 """pytest configuration
 
-tests 폴더 내의 모든 test code 실행 방법
-
+tests 폴더 내의 모든 test code 실행 방법\n
 - root directory 이동
-- python3 -m tests 입력
+- python -m pytest tests (tests 폴더 내 test_*.py 또는 *_test.py 파일 모두 실행)
+
+일부 test code 실행 방법\n
+- python -m pytest {디렉토리명}/{테스트파일명}.py
+
+pytest option (https://velog.io/@sangyeon217/pytest-execution)
+- k 옵션
+    - pytest {테스트파일명}.py -k {테스트함수명} // 특정 테스트 함수만 실행
+- v 옵션
+    - 기본 실행 명령어는 Fail => F, pass => . 으로만 표시하는데 여기선 각 테스트 함수 실행 결과 출력
+- vv 옵션
+- s 옵션
+- r 옵션
 
 중복 class 공유 방법
 
@@ -15,7 +26,7 @@ def class():
     class = Class() \n
     return class \n
 
-- test_code.py
+- test_model.py
 def test_func1(class):
     '''Test functionality of func1'''
     asset class.func1(...) == some_value
---
+++ tests/test_model.py
@@ -0,0 +1,3 @@
+def test_something1():
+    print("Hello!!!!!!!")
+    assert 1 == 2
